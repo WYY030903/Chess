bdeabfc22e10909e5d48ea18101a001c1ff29700
WYY030903 <12111641@mail.sustech.edu.cn>
2022-05-23 16:17:20 +0800

    Final

--- ChessDemo/src/view/ChessGameFrame.java
+++ ChessDemo/src/view/ChessGameFrame.java
@@ -2,10 +2,12 @@ package view;
 
 import controller.GameController;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * 这个类表示游戏过程中的整个游戏界面，是一切的载体
@@ -43,6 +45,8 @@ public class ChessGameFrame extends JFrame {
         addUndoButton();
         addPicture();
 
+        new Thread(()->{while(true) {playMusic();} //while中的true可换成参数来控制音乐的停止播放
+        }).start();
 
     }
 
@@ -178,12 +182,39 @@ public class ChessGameFrame extends JFrame {
         Container container;
 
         JLabel image;
-        container = this.getContentPane();
+        container=this.getContentPane();
 
-        container.add(image = new JLabel(new ImageIcon("./images/picture.png")));
-        image.setLocation(HEIGTH / 100, HEIGTH / 100);
-        image.setBounds(10, 10, 735, 735);
+        container.add(image= new JLabel(new ImageIcon("./images/picture.png")));
+        image.setLocation(HEIGTH/100,HEIGTH/100);
+        image.setBounds(10,10,735,735);
         this.setVisible(true);
     }
 
+    private void playMusic() {// 背景音乐播放
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(new File("./images/陈致逸 - The Edge of the Prairie 平原的边际.wav"));    //绝对路径
+            AudioFormat aif = ais.getFormat();
+            final SourceDataLine sdl;
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, aif);
+            sdl = (SourceDataLine) AudioSystem.getLine(info);
+            sdl.open(aif);
+            sdl.start();
+            FloatControl fc = (FloatControl) sdl.getControl(FloatControl.Type.MASTER_GAIN);
+            // value可以用来设置音量，从0-2.0
+            double value = 2;
+            float dB = (float) (Math.log(value == 0.0 ? 0.0001 : value) / Math.log(10.0) * 20.0);
+            fc.setValue(dB);
+            int nByte = 0;
+            final int SIZE = 1024 * 64;
+            byte[] buffer = new byte[SIZE];
+            while (nByte != -1) {
+                nByte = ais.read(buffer, 0, SIZE);
+                sdl.write(buffer, 0, nByte);
+            }
+//            sdl.stop();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
